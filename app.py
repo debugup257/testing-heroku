819fcc16ef5a40e74a468586d4710089cc7822a8
debugup257 <99998735+debugup257@users.noreply.github.com>
2022-08-16 22:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, render_template_string, request,redirect,url_for,session
 import psycopg2
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -10,6 +13,35 @@ c = conn.cursor()
 
 @app.route('/')
 def index():
+   if request.method=="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        c.execute("SELECT username FROM users;")
+        usernames = (c.fetchall())
+        usernames_list=[]
+
+        for i in usernames:
+            usernames_list.append(i[0])
+        conn.commit()
+        
+        c.execute("SELECT password FROM users;")
+        passwords = (c.fetchall())
+        passwords_list=[]
+
+        for i in passwords:
+            passwords_list.append(i[0])
+        conn.commit()
+
+        if username in usernames_list:
+            if password in passwords_list:
+                session.permanent=False
+                session["user"]=username
+                c.execute("""SELECT user_type FROM users WHERE username = %(value)s; """,{"value":username})
+                user_type = (c.fetchall())
+                if user_type[0][0]=="admin":
+                    return "success"
+
     return render_template("index.html")
 
 if __name__ == '__main__':
