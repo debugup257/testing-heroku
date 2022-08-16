baeff919e162f3ee41fdb7816bac1bb24dce5daa
debugup257 <99998735+debugup257@users.noreply.github.com>
2022-08-16 20:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template
+import psycopg2
 
 app = Flask(__name__)
+
+conn = psycopg2.connect(
+   database="zsykrjpg", user='zsykrjpg', password='hTl4Mjgl4Wl0QOmHLragl010RLsgN0eb', host='tiny.db.elephantsql.com', port= '5432'
+)
+c = conn.cursor()
 
 @app.route('/')
 def index():
