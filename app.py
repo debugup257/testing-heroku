4d655587e59d444ea235b4c2d656f867ce2cab3c
debugup257 <99998735+debugup257@users.noreply.github.com>
2022-08-16 20:59:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import psycopg2
 
 app = Flask(__name__)
 
-# conn = psycopg2.connect(
-#    database="zsykrjpg", user='zsykrjpg', password='hTl4Mjgl4Wl0QOmHLragl010RLsgN0eb', host='tiny.db.elephantsql.com', port= '5432'
-# )
-# c = conn.cursor()
+conn = psycopg2.connect(
+   database="d67fsm4svq5gp3", user='cthlqzrfduldux', password='284d42f2d7277cf2318c7053bb11f6665c3ba385f1abc9ca3668af049a5eb06e', host='ec2-44-195-100-240.compute-1.amazonaws.com', port= '5432'
+)
+c = conn.cursor()
 
 @app.route('/')
 def index():
